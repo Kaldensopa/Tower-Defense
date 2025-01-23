5fb805d335e4a96b526e0295a36c31d6a95befae
Kaldensopa <160997098+Kaldensopa@users.noreply.github.com>
2025-01-22 20:06:11 -0600

    Add files via upload

---
+++ enemy_data.py
@@ -0,0 +1,162 @@
+ENEMY_SPAWN_DATA = [
+  {
+    #1
+    "weak": 15,
+    "medium": 0,
+    "strong": 0,
+    "elite":1
+  },
+  {
+    #2
+    "weak": 30,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #3
+    "weak": 20,
+    "medium": 5,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #4
+    "weak": 30,
+    "medium": 15,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #5
+    "weak": 5,
+    "medium": 20,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #6
+    "weak": 15,
+    "medium": 15,
+    "strong": 4,
+    "elite": 0
+  },
+  {
+    #7
+    "weak": 20,
+    "medium": 25,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #8
+    "weak": 10,
+    "medium": 20,
+    "strong": 15,
+    "elite": 0
+  },
+  {
+    #9
+    "weak": 15,
+    "medium": 10,
+    "strong": 5,
+    "elite": 0
+  },
+  {
+    #10
+    "weak": 0,
+    "medium": 100,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #11
+    "weak": 5,
+    "medium": 10,
+    "strong": 12,
+    "elite": 2
+  },
+  {
+    #12
+    "weak": 0,
+    "medium": 15,
+    "strong": 10,
+    "elite": 5
+  },
+  {
+    #13
+    "weak": 20,
+    "medium": 0,
+    "strong": 25,
+    "elite": 10
+  },
+  {
+    #14
+    "weak": 15,
+    "medium": 15,
+    "strong": 15,
+    "elite": 15
+  },
+  {
+    #15
+    "weak": 25,
+    "medium": 25,
+    "strong": 25,
+    "elite": 25
+  },
+  {
+    #16
+    "weak": 30,
+    "medium": 30,
+    "strong": 30,
+    "elite": 30
+  },
+  
+  {
+    #17
+    "weak": 35,
+    "medium": 40,
+    "strong": 40,
+    "elite": 40
+  },
+  {
+    #18
+    "weak": 40,
+    "medium": 40,
+    "strong": 40,
+    "elite": 40
+  },
+  {
+    #19
+    "weak": 45,
+    "medium": 45,
+    "strong": 45,
+    "elite": 45
+  },
+  {
+    #20
+    "weak": 100,
+    "medium": 100,
+    "strong": 100,
+    "elite": 100
+  }
+]
+
+ENEMY_DATA = {
+    "weak": {
+    "health": 10,
+    "speed": 2
+  },
+    "medium": {
+    "health": 15,
+    "speed": 3
+  },
+    "strong": {
+    "health": 20,
+    "speed": 4
+  },
+    "elite": {
+    "health": 50,
+    "speed": 4
+  }
+}
---
+++ turret_data.py
@@ -0,0 +1,22 @@
+TURRET_DATA = [
+  {
+    #1
+    "range": 150,
+    "cooldown": 1400,
+  },
+  {
+    #2
+    "range": 180,
+    "cooldown": 1100,
+  },
+  {
+    #3
+    "range": 200,
+    "cooldown": 900,
+  },
+  {
+    #4
+    "range": 225,
+    "cooldown": 800,
+  }
+]
